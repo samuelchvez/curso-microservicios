9d810eb58630e2442d1eee02ab85b458ce46315f
Samuel Chávez <schavezfuentes@gmail.com>
2021-02-04 12:18:35 -0600

    Get signed URL

---
+++ getSignedURL.js
@@ -0,0 +1,25 @@
+const S3 = require('aws-sdk/clients/s3');
+
+
+exports.handler = async (event) => {
+    const { body } = event;
+    const { pictureName } = JSON.parse(body);
+    
+    const s3 = new S3({ signatureVersion: 'v4' });
+    const signedURL = s3.getSignedUrl(
+        'getObject',
+        {
+            Bucket: 'cursomicroservicios-dia2',
+            Key: 'images/' + pictureName,
+            Expires: 300,
+        },
+    );
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            signedURL,
+        }),
+    };
+};
+
